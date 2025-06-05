7821dceadf7c8f9d89b73fffd5f48603b12612cc
Iban Blondelle <ibanblondelle@gmail.com>
2025-06-05 14:15:24 +0200

    first commit, added console app

---
+++ src/AnomalyDetection/Program.cs
@@ -0,0 +1,28 @@
+﻿using CsvHelper;
+using AnomalyDetection.Model;
+
+using CsvHelper.Configuration;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+
+var filePath = Path.Combine("data", "wateralarm-vives.csv");
+
+using var reader = new StreamReader(filePath, Encoding.UTF8);
+using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+    HeaderValidated = null,
+    MissingFieldFound = null
+});
+
+var records = csv.GetRecords<SensorData>().ToList();
+
+// Print the records
+foreach (var record in records)
+{
+    Console.WriteLine($"Tag = {record.tags}, Distance = {record.distance} mm");
+}
+
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
---
+++ src/AnomalyDetection/model/SensorData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AnomalyDetection.Model
+{
+    public class SensorData
+    {
+        public string? name { get; set; }
+        public string? tags { get; set; }
+        public DateTime time { get; set; }
+        public double? BatV { get; set; }
+        public int? RSSI { get; set; }
+        public double? batv { get; set; }
+        public int? distance { get; set; }
+        public int? distance_raw { get; set; }
+        public string? level { get; set; }
+        public string? liter { get; set; }
+    }
+}
+
+
+
