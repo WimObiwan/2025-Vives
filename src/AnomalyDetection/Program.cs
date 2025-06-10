c682943cd7a397642aa14feb1f0c74c9e23c65a0
Tijl Debacker <tijl@codekick.be>
2025-06-10 10:57:46 +0200

    git issues + beetje nieuwe code die nog nie kan sorteren of zo

--- src/AnomalyDetection/Program.cs
+++ src/AnomalyDetection/Program.cs
@@ -5,6 +5,7 @@ using CsvHelper.Configuration;
 using System.Globalization;
 using System.IO;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 
 var filePath = Path.Combine("data", "wateralarm-vives.csv");
@@ -22,6 +23,24 @@ var records = csv.GetRecords<SensorData>().ToList();
 foreach (var record in records)
 {
     Console.WriteLine($"Tag = {record.tags}, Distance = {record.distance} mm");
+}
+
+int i = 0;
+var tag = records[0].Tag;
+
+//print the anomalies
+foreach (var record in records)
+{ //toevoegen: checken tag, iqr per tag berekenen, bools iqr en z, writeline definite anomaly
+    if (record.Tag != tag)
+    {
+        tag = record.Tag;
+    }
+
+    if (record != records[0] && i+2 < records.Length) {
+        
+        Console.WriteLine($"!POSSIBLE ANOMALY! Tag = {record.tags}, Time= {record.time}, Distance = {record.distance} mm");
+    }
+    i++;
 }
 
 Console.WriteLine("\nPress any key to exit...");
