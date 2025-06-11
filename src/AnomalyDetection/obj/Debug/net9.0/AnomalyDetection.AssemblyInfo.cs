5d17eb09bd6ab51d1163d577dd4bf6dcae8aac01
Iban Blondelle <ibanblondelle@gmail.com>
2025-06-11 11:43:31 +0200

    added CsvHelper package

--- src/AnomalyDetection/obj/Debug/net9.0/AnomalyDetection.AssemblyInfo.cs
+++ src/AnomalyDetection/obj/Debug/net9.0/AnomalyDetection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnomalyDetection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eeafb24e6b3836a8ae50440eb19a42285e6bde5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0643e48d63c85962bc2f37f5c8a529cc0cca490d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnomalyDetection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnomalyDetection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/AnomalyDetection/obj/Release/net9.0/AnomalyDetection.AssemblyInfo.cs
+++ src/AnomalyDetection/obj/Release/net9.0/AnomalyDetection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnomalyDetection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c682943cd7a397642aa14feb1f0c74c9e23c65a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0643e48d63c85962bc2f37f5c8a529cc0cca490d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnomalyDetection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnomalyDetection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
